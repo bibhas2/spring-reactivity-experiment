bd2f68215ed7e9c18368f8eb212b2976c251bf4c
Bibhas Bhattacharya <7002714@C02W66D1HTD5.local>
2019-02-22 16:32:46 -0500

    Simplify array type handling

--- demo-mvc/src/main/java/com/example/demomvc/SimpleController.java
+++ demo-mvc/src/main/java/com/example/demomvc/SimpleController.java
@@ -1,14 +1,11 @@
 package com.example.demomvc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.GenericType;
-import java.util.List;
 import java.util.concurrent.CompletionStage;
 
 @RestController
@@ -18,12 +15,12 @@ public class SimpleController {
             .target("https://jsonplaceholder.typicode.com/");
 
     @GetMapping("/blog-post")
-    public CompletionStage<List<BlogPost>> getBlogPosts() {
+    public CompletionStage<BlogPost[]> getBlogPosts() {
         return target
             .path("/posts")
             .queryParam("userId", 1)
             .request()
             .rx()
-            .get(new GenericType<List<BlogPost>>() { });
+            .get(BlogPost[].class);
     }
 }
